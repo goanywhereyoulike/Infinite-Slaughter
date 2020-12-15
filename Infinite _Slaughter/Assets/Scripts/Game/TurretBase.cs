75f0065b87d6f804e62838ff49e6644955239ea5
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2020-12-14 23:34:38 -0800

    Revert "Merge branch 'main' into Audio"
    This reverts commit 79e6dbb0706d84f90773b811f86c8a6ed98e28dc, reversing
    changes made to a226991fa3171a88ea5d796bd5bae753874a820e.

--- Infinite _Slaughter/Assets/Scripts/Game/TurretBase.cs
+++ Infinite _Slaughter/Assets/Scripts/Game/TurretBase.cs
@@ -12,7 +12,6 @@ public class TurretBase : MonoBehaviour
     GameObject buildLocation;
     public float range;
     bool isInRange;
-    bool isBuilt;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,23 +19,22 @@ public class TurretBase : MonoBehaviour
         turret.gameObject.SetActive(false);
         buildUI.gameObject.SetActive(false);
         isInRange = false;
-        isBuilt = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.E) && isInRange)
+        if (Input.GetKey(KeyCode.E)&&isInRange)
         {
             turret.gameObject.SetActive(true);
             buildLocation.gameObject.SetActive(false);
-            isBuilt = true;
+            Destroy(buildUI.gameObject);
 
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !isBuilt)
+        if (other.CompareTag("Player"))
         {
             buildUI.gameObject.SetActive(true);
             isInRange = true;
@@ -45,7 +43,7 @@ public class TurretBase : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player") && !isBuilt)
+        if (other.CompareTag("Player"))
         {
             buildUI.gameObject.SetActive(false);
             isInRange = false;
