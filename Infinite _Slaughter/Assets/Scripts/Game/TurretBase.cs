6f5324b258c23b51d3fe1dd58db1cff1943d6d3d
Steven Gan <stevenganyl@gmail.com>
2020-12-14 22:05:35 -0800

    BuildSystem and basic turret Complete
    -Player will see notice to build turret when get closer
    -click e will destroy the notification and build a turret

---
+++ Infinite _Slaughter/Assets/Scripts/Game/TurretBase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBase : MonoBehaviour
+{
+    [SerializeField]
+    GameObject turret;
+    [SerializeField]
+    Canvas buildUI;
+    [SerializeField]
+    GameObject buildLocation;
+    public float range;
+    bool isInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SphereCollider>().radius = range;
+        turret.gameObject.SetActive(false);
+        buildUI.gameObject.SetActive(false);
+        isInRange = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E)&&isInRange)
+        {
+            turret.gameObject.SetActive(true);
+            buildLocation.gameObject.SetActive(false);
+            Destroy(buildUI.gameObject);
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            buildUI.gameObject.SetActive(true);
+            isInRange = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            buildUI.gameObject.SetActive(false);
+            isInRange = false;
+        }
+    }
+}
