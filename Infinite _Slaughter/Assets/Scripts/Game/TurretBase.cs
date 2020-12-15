95b58517df5df01730beeb1f31c9e0b8c5598a4a
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2020-12-14 23:18:37 -0800

    TestCommitTurret

---
+++ Infinite _Slaughter/Assets/Scripts/Game/TurretBase.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBase : MonoBehaviour
+{
+    [SerializeField]
+    GameObject turret;
+    [SerializeField]
+    Canvas buildUI;
+    [SerializeField]
+    GameObject buildLocation;
+    public float range;
+    bool isInRange;
+    bool isBuilt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SphereCollider>().radius = range;
+        turret.gameObject.SetActive(false);
+        buildUI.gameObject.SetActive(false);
+        isInRange = false;
+        isBuilt = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && isInRange)
+        {
+            turret.gameObject.SetActive(true);
+            buildLocation.gameObject.SetActive(false);
+            isBuilt = true;
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isBuilt)
+        {
+            buildUI.gameObject.SetActive(true);
+            isInRange = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && !isBuilt)
+        {
+            buildUI.gameObject.SetActive(false);
+            isInRange = false;
+        }
+    }
+}
