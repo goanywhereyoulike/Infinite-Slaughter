d7d59b83dfd773dde71e95ec1f528c6789550720
震 方 <a1521442784@live.com>
2020-12-15 20:05:40 -0800

    [game engine] -add finalzone

---
+++ Infinite _Slaughter/Assets/Scripts/Game/FinalZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalZone : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = FindObjectOfType<PlayerController>();
+        DestructibleObject enemy = other.gameObject.GetComponent<DestructibleObject>();
+        if (enemy != null)
+        {
+            enemy.CurrentHealth = 0;
+            player.gameObject.GetComponent<DestructibleObject>().TakeDamage(10);
+            ServiceLocator.Get<GameManager>().UpdateHealthBar(player.gameObject.GetComponent<DestructibleObject>().CurrentHealth);
+
+        }
+    }
+}
